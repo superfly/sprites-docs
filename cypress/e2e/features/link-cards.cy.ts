6e6ab6e31f1436bc98badf1a66bb958a1c87bded
Kyle McLaren <mclaren@fly.io>
2026-01-03 20:45:58 +0200

    Add LinkCard components for related documentation sections (#32)

---
+++ cypress/e2e/features/link-cards.cy.ts
@@ -0,0 +1,29 @@
+describe('LinkCards', () => {
+  beforeEach(() => {
+    cy.visit('/quickstart');
+  });
+
+  it('should render cards with icon, title, and description', () => {
+    cy.get('.grid a.no-underline')
+      .first()
+      .within(() => {
+        cy.get('svg').should('exist');
+        cy.get('h3').should('exist').and('not.be.empty');
+        cy.get('p').should('exist').and('not.be.empty');
+      });
+  });
+
+  it('should have valid navigation links', () => {
+    cy.get('.grid a.no-underline')
+      .first()
+      .then(($link) => {
+        const href = $link.attr('href');
+        // Verify href exists and is an internal link
+        expect(href).to.match(/^\//);
+        // Verify linked page exists (returns 200)
+        cy.request(href as string)
+          .its('status')
+          .should('eq', 200);
+      });
+  });
+});
